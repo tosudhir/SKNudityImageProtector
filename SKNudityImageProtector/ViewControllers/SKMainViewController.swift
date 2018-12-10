515de571f3a0bc7dd585cca964ba23d7d21d8757
tosudhir <to.sudhir1981@gmail.com>
2018-12-10 19:04:55 +0530

    image added.

---
+++ SKNudityImageProtector/ViewControllers/SKMainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SKMainViewController.swift
+//  SKNudityImageProtector
+//
+//  Created by Sudhir Kumar on 01/11/18.
+//  Copyright © 2018 SAN. All rights reserved.
+//
+
+import UIKit
+
+class SKMainViewController: UIViewController {
+    
+    //IBOutlets
+    
+    //Public iVars
+    
+    //Private iVars
+    
+    //Public Computed iVars
+    
+    //Private Computed iVars
+}
+
+// MARK: - View Life Cycle
+extension SKMainViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+// MARK: - Private Functions
+extension SKMainViewController {
+    
+    fileprivate func setupUI() {
+        
+    }
+}
+
+// MARK: - IBActions
+extension SKMainViewController {
+}
